6c3e84ea0072818895fe61f7c035aba3a940b99f
qunnderrie1 <qunnderrie@gmail.com>
2024-01-03 00:32:05 -0500

    made changes

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use((err, req, res, next) => {
 
 
 app.get("*" , (req, res) => {
-    res.sendFile(path.join(__dirname , 'client', 'public' , 'index.html'))
+    res.sendFile(path.join(__dirname , 'client', 'dist' , 'index.html'))
 
 
 })
