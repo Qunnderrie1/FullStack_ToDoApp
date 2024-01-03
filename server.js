2259a709772f0f9e743675cc115f3cf238d903a7
qunnderrie1 <qunnderrie@gmail.com>
2024-01-03 00:04:41 -0500

    made changes

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'Client/dist')))
+app.use(express.static(path.join(__dirname, 'app/dist')))
 
 connectDB();
 
@@ -44,7 +44,7 @@ app.use((err, req, res, next) => {
 
 
 app.get("*" , (req, res) => {
-    res.sendFile(path.join(__dirname , 'Client', 'dist' , 'index.html'))
+    res.sendFile(path.join(__dirname , 'app', 'dist' , 'index.html'))
 
 
 })
